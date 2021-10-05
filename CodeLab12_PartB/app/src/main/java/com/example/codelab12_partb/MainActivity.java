3ddb1af7f514dc7ffda857b954d3b10223b4e83b
chiencodon <chienbn9x@gmail.com>
2021-10-05 14:20:17 +0700

    code lab 1.2 part A and B

---
+++ CodeLab12_PartB/app/src/main/java/com/example/codelab12_partb/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.codelab12_partb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view)
+    {
+
+    }
+
+    public void countUp(View view)
+    {
+
+    }
+
+}
